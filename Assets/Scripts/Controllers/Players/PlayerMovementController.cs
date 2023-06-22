f56f12815db2ad1ad61f0aab33396709f25ca93c
OnurHG <onurhangumus@gmail.com>
2023-06-22 18:42:38 +0300

    -Zenject and starter package is imported to project.

---
+++ Assets/Scripts/Controllers/Players/PlayerMovementController.cs
@@ -0,0 +1,40 @@
+﻿using Data.MetaData;
+using Events.External;
+using Signals;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+
+namespace Controllers
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [Inject] private PlayerSettings PlayerSettings { get; set; }
+
+        private RoutineHelper _onPosUpdate;
+        private bool _isStarted = false;
+        private Settings _mySettings;
+        
+        private void Awake()
+        {
+            _mySettings = PlayerSettings.PlayerMovementSettings;
+        }
+
+        public void OnPlay()
+        {
+            _isStarted = true;
+            Debug.Log(this.name);
+        }
+        public void OnRestartLevel()
+        {
+            _isStarted = false;
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            [SerializeField] public float Speed = 1f;
+        }
+    }
+}

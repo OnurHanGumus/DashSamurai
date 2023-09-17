99a283d781a03b499da56e5ee53cd413f13d400f
OnurHG <onurhangumus@gmail.com>
2023-09-17 10:05:32 +0300

    no message

--- Assets/AdditionalPackages/AurynSky/Forest Pack/Scripts/MaterialMover.cs
+++ Assets/AdditionalPackages/AurynSky/Forest Pack/Scripts/MaterialMover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MaterialMover2 : MonoBehaviour
+public class MaterialMover : MonoBehaviour
 {
 
     public float scrollSpeed = 0.5F;

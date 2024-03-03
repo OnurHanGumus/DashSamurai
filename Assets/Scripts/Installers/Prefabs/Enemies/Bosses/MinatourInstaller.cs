b8ca82c3a1da217d9d42394030756825c4f8e6c1
OnurHG <onurhangumus@gmail.com>
2024-03-03 14:45:31 +0300

    -minotour boss prefab is created

---
+++ Assets/Scripts/Installers/Prefabs/Enemies/Bosses/MinatourInstaller.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+using Controllers;
+
+namespace Installers.Prefabs
+{
+    public class MinatourInstaller : EnemyInstaller
+    {
+        public override void InstallBindings()
+        {
+            BindSignals();
+            BindData();
+            BindReferences();
+            BindConditions();
+            BindTransitions();
+            BindStates();
+            BindStateMachine();
+        }
+
+        protected override void BindSignals()
+        {
+            base.BindSignals();
+        }
+
+        protected override void BindData()
+        {
+            _enemySettings = Resources.Load<EnemySettings>("Data/Bosses/MinatourSettings");
+            Container.BindInstance(_enemySettings).AsSingle();
+        }
+
+        protected override void BindReferences()
+        {
+            base.BindReferences();
+        }
+
+        protected override void BindConditions()
+        {
+            base.BindConditions();
+        }
+
+        protected override void BindTransitions()
+        {
+            base.BindTransitions();
+        }
+
+        protected override void BindStates()
+        {
+            base.BindStates();
+        }
+
+        protected override void BindStateMachine()
+        {
+            base.BindStateMachine();
+        }
+    }
+}
